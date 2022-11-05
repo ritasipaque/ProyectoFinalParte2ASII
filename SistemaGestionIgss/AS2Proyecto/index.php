0c5d9293b06444cdb89e031357ead0a92b44607c
NydiLion <snaker2010@hotmail.es>
2022-11-04 22:30:51 -0600

    subiendo agendar cita

---
+++ SistemaGestionIgss/AS2Proyecto/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Agendar Cita Vacuna</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body>
+				  <div class="p-3 mb-2 bg-primary text-white"><h2>Agendar Cita COVID-19</h2></div>
+				  <div class="card-group">
+					<div class="card">
+					  <img src="imgn/L1.jpg" class="card-img-top" alt="...">
+
+
+
+    <form action ="agendarcita.php" method="post">
+    	<h1>¡Vacunate!</h1>
+		<h1>¡Agenda tu cita para tu vacuna COVID-19!</h1>
+    	<input type="text" name="centro" placeholder="Centro de Vacunacion" >
+		<input type="date" name="fechaC" placeholder="Fecha Cita Vacuna">
+    	<input type="text" name="vacuna" placeholder="Nombre Vacuna" >
+		<input type="text" name="lote" placeholder="Lote Vacuna" >
+    	<input type="submit" name="register">	
+    </form>
+	
+	
+	
+
+
+
+	<div class="p-3 mb-2 bg-primary text-white">
+		7a. Avenida 22-72 zona 01,
+		Ciudad de Guatemala, Guatemala.   Call Center: 1522
+		PBX: 2297-1224 / 2412-1224</div>
+	  
+</body>
+</html>
